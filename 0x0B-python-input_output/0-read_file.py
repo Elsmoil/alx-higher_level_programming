b418260df83be8f0430a1640556da033f6c6d13f
Elsmoil <alsmoilalrasta999@gmail.com>
2024-02-06 07:17:22 -0800

    more

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-def read_file(filename=""):
+"""Defines a text file-reading function."""
 
-    """Reads a text file (UTF8) and prints it to stdout."""
-    with open(filename, encoding='utf-8') as file:
-    print(file.read(), end='')
+
+def read_file(filename=""):
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
