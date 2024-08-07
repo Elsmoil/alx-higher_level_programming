436d5e61f79d6084e3a2a2ce82dfe3379cd8e8a0
Elsmoil <alsmoalsuliman@gmail.com>
2024-08-07 13:26:40 -0700

    more

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,5 @@
 -- write a script print the full description of the table
 
-SELECT column_name, column_type, is_nullable, column_key, column_default, extra FROM information_schema.columns WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
+
+--SELECT column_name, column_type, is_nullable, column_key, column_default, extr--a FROM information_schema.columns WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_N--AME = 'first_table';
