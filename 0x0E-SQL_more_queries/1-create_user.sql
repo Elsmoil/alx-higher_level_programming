46ff2431510d0cde38de78231540e695f4663451
Elsmoil <alsmoalsuliman@gmail.com>
2024-08-22 15:12:22 -0700

    newww

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
---A script that creates the mysql server user user_0d_1
+-- Script that creates the mysql server user
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
