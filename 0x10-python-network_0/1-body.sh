a1a6065d27ea24911f6f8023604b5e1e5bab9e16
Elsmoil <alsmoalsuliman@gmail.com>
2024-08-29 09:06:10 -0700

    more test

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-#Write a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-
-response=$(curl -s -w "%{http_code}\n" $1)
-status_code=$(echo "$response" | tail -n 1)
-body=$(echo "$response" | sed '$ d')
-
-if [ "$status_code" = "200" ]; then
-    echo "$body"
-fi
+# Send a GET request to a URL and display the response body if the status code is 200
+curl -s -w "%{http_code}\n" $1 | (read body; read code; if [ "$code" = "200" ]; then echo "$body"; fi)
